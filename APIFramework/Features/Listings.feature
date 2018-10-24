1b324f063e8cae83cecf53ebdf2054a56dad4770
Richard <r.j.howard.01@gmail.com>
2018-10-24 14:17:14 +0200

    Initial commit

---
+++ APIFramework/Features/Listings.feature
@@ -0,0 +1,10 @@
+﻿Feature: Classifieds
+	My feature to validate classified listings
+
+Scenario: Confirm classified listing for Carbon credits
+	Given I have called the following api: 
+	|	endpoint																	|
+	|	https://api.tmsandbox.co.nz/v1/Categories/6327/Details.json?catalogue=false |
+	Then the response should contain the field Name with the value set to Carbon credits
+	Then the response should contain the field CanRelist with the value set to true
+	Then the Promotions element with the Name Gallery, should have a child Description field that contains the text 2x larger image
---
+++ APIFramework/Steps/ListingsSteps.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+using System.Configuration;
+using System.Collections.Generic;
+using APIFramework.Utils;
+using System.Threading;
+using APIFramework;
+using Newtonsoft.Json;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using RestSharp;
+using System.Net.Http.Headers;
+using NUnit.Framework;
+
+namespace APIFramework
+{
+
+    [Binding]
+    public class ListingsSteps
+    {
+
+        public static JToken rsp;
+
+        [Given(@"I have called the following api:")]
+        public void GivenIHaveNavigatedToTheSpecifiedAPI(Table table)
+        {
+            //Store parameters in dictionary table
+            var api = table.CreateInstance<API>();
+            var client = new RestClient(api.Endpoint);
+            var request = new RestRequest();
+            System.Net.ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+            IRestResponse response = client.Execute(request);
+
+            //Store response in a variable that can be accessed by other methods
+            rsp = JToken.Parse(response.Content);
+            
+        }
+        
+        [Then(@"the response should contain the field (.*) with the value set to (.*)")]
+        public void ThenTheResponseShouldContain(string field, string value)
+        {
+            //loop through each element in response
+            foreach (JProperty x in rsp)
+            {
+                if (x.Name == field)
+                {
+                    switch (x.Value.Type)
+                    {
+                        case JTokenType.Boolean:
+                            Assert.AreEqual((bool)x.Value, Convert.ToBoolean(value));
+                            return;
+                        case JTokenType.String:
+                            Assert.AreEqual((string)x.Value, value);
+                            return;
+                        case JTokenType.Integer:
+                            Assert.AreEqual((int)x.Value, Convert.ToInt64(value));
+                            return;
+                        case JTokenType.Float:
+                            Assert.AreEqual((float)x.Value, float.Parse(value));
+                            return;
+                    }
+                }
+            }
+            //Case for when field is not found in response
+            Assert.AreEqual(null, value);
+        }
+
+
+        [Then(@"the (.*) element with the (.*) (.*), should have a child (.*) field that contains the text (.*)")]
+        public void ThenTheResponseShouldContain(string element, string field, string value, string childfield, string childvalue)
+        {
+            string foundfieldvalue = String.Empty;
+
+            foreach (var child in rsp[element].Children())
+            {
+                if ((string)child.SelectToken(field) == value)
+                {
+                    foundfieldvalue = (string)child.SelectToken(childfield);
+                    break;
+                }
+
+            }
+            Assert.IsTrue(foundfieldvalue.Contains(childvalue));
+        }
+
+    }
+}
